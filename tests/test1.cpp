d8df70540882c840904d7628d3937089c223e530
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-05-19 15:18:18 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -7,14 +7,14 @@
 
 
 TEST_CASE("Creating tree") {
-    BStree::Tree<int> tree;
+    BSTree::Tree<int> tree;
     REQUIRE(tree.empty_tree() == false);
-    BStree::Tree<int> tree_{1};
+    BSTree::Tree<int> tree_{1};
     REQUIRE(tree_.empty_tree() == true);
 }
 
 TEST_CASE("Printing of tree by various traversal order") {
-    BStree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
+    BSTree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
     std::string pre = "8  4  2  1  3  6  5  7  12  10  9  11  14  13  15  ";
     std::string in = "1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  ";
     std::string post = "1  3  2  5  7  6  4  9  11  10  13  15  14  12  8  ";
@@ -22,11 +22,11 @@ TEST_CASE("Printing of tree by various traversal order") {
     std::string str;
     std::stringstream out(str);
 
-    tree.print_units(out, BStree::traversal_order::pre);
+    tree.print_units(out, BSTree::traversal_order::pre);
     out<<std::endl;
-    tree.print_units(out, BStree::traversal_order::in);
+    tree.print_units(out, BSTree::traversal_order::in);
     out<<std::endl;
-    tree.print_units(out, BStree::traversal_order::post);
+    tree.print_units(out, BSTree::traversal_order::post);
     out<<std::endl;
 
     std::string result_pre;
@@ -43,7 +43,7 @@ TEST_CASE("Printing of tree by various traversal order") {
 }
 
 TEST_CASE("Addig node") {
-    BStree::Tree<int> tree = { 25, 34, 12, 4, 7, 67, 78, 9, 0 };
+    BSTree::Tree<int> tree = { 25, 34, 12, 4, 7, 67, 78, 9, 0 };
 
     tree.insert(1);
     tree.insert(20);
@@ -53,7 +53,7 @@ TEST_CASE("Addig node") {
     std::string str;
     std::stringstream out(str);
 
-    tree.print_order(out, BStree::traversal_order::pre);
+    tree.print_order(out, BSTree::traversal_order::pre);
 
     std::string result = "25  12  4  0  1  7  5  9  20  34  67  78  ";
     std::string result_of_adding;
@@ -63,7 +63,7 @@ TEST_CASE("Addig node") {
 }
 
 TEST_CASE("Deleting node") {
-    BStree::Tree<int> tree = { 25, 34, 12, 4, 7, 67, 78, 9, 0 };
+    BSTree::Tree<int> tree = { 25, 34, 12, 4, 7, 67, 78, 9, 0 };
 
     tree.delete_units(25);
     tree.delete_units(0);
@@ -74,7 +74,7 @@ TEST_CASE("Deleting node") {
     std::string str;
     std::stringstream out(str);
 
-    tree.print_units(out, BStree::traversal_order::pre);
+    tree.print_units(out, BSTree::traversal_order::pre);
 
     std::string result_of_deleting;
     getline(out, result_of_deleting);
@@ -83,7 +83,7 @@ TEST_CASE("Deleting node") {
 }
 
 TEST_CASE("Input in file") {
-    BStree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
+    BSTree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
 
     std::string text = "8  4  2  1  3  6  5  7  12  10  9  11  14  13  15  ";
     std::string buffer;
@@ -98,14 +98,14 @@ TEST_CASE("Input in file") {
 }
 
 TEST_CASE("Existence of node") {
-    BStree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
+    BSTree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
     REQUIRE(tree.exists(8) == true);
     REQUIRE(tree.exists(100) == false);
 }
 
 TEST_CASE("Saving in and loading from file") {
-    BStree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
-    BStree::Tree<int> tree1;
+    BSTree::Tree<int> tree = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
+    BSTree::Tree<int> tree1;
 
     std::ofstream fout("answer.txt");
     fout << "Да";
