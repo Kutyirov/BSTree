1849d9aac35630758e85199b120f197472310784
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-05-19 22:03:09 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -10,8 +10,8 @@
 TEST_CASE("Creating tree") {
 	BSTree::Tree tree;
 	REQUIRE(tree.empty_tree() == true);
-	BSTree::Tree tree_{ 1 };
-	REQUIRE(tree_.empty_tree() == false);
+	BSTree::Tree tree{ 1 };
+	REQUIRE(tree.empty_tree() == false);
 }
 
 TEST_CASE("Creating tree2") {
