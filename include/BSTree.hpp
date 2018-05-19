1c18c24e12ae997f8ff2a3a8f303c1021d1f98ed
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-05-19 15:51:20 +0300

    Update BSTree.hpp

--- include/BSTree.hpp
+++ include/BSTree.hpp
@@ -26,10 +26,9 @@ class Tree {
   //параметр - кол-во нулей для
   //текущего узла
 
-  auto print_direct(std::ostream& out, const BStree::Node* node) -> std::ostream&;  //вывод 3-х обходов
-  auto print_symmetric(std::ostream& out, const BStree::Node* node) -> std::ostream&;
-  auto print_reverse(std::ostream& out, const BStree::Node* node) -> std::ostream&;
-  // auto copy(Node*) -> void;  //копирование дерева
+  auto print_direct(std::ostream& out, const BStree::Node* node) const-> std::ostream&;  //вывод 3-х обходов
+  auto print_symmetric(std::ostream& out, const BStree::Node* node) const-> std::ostream&;
+  auto print_reverse(std::ostream& out, const BStree::Node* node) const-> std::ostream&;
   auto save(std::ostream&, Node*) const
       -> void;  //перегрузка сохранения для рекурсии
 
