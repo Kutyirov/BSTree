d38e4a3d23c46b8573ee7119648fb71ac0610ba3
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-05-20 16:10:43 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -16,6 +16,7 @@ TEST_CASE("Creating tree2") {
 	  BSTree::Tree tree{ 1 };
 	  REQUIRE(tree.empty_tree() == false);
 }
+
 TEST_CASE("TRAVERSAL ORDER DIRECT") {
     	BSTree::Tree tree = { 10, 4, 3, 9, 15, 6, 12, 1, 11, 8, 2, 14, 7, 5, 13};
     	std::string dir = "10 4 3 1 2 9 6 5 8 7 15 12 11 14 13 ";
@@ -27,6 +28,7 @@ TEST_CASE("TRAVERSAL ORDER DIRECT") {
     	getline(out, result_dir);
     	REQUIRE(dir == result_dir);
 }
+
 TEST_CASE("TRAVERSAL ORDER SYMMETRIC") {
 	BSTree::Tree tree = { 10, 4, 3, 9, 15, 6, 12, 1, 11, 8, 2, 14, 7, 5, 13};
 	std::string sym = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ";
@@ -38,6 +40,7 @@ TEST_CASE("TRAVERSAL ORDER SYMMETRIC") {
 	getline(out, result_sym);
 	REQUIRE(sym == result_sym);
 }
+
 TEST_CASE("TRAVERSAL ORDER REVERSE") {
 	BSTree::Tree tree = { 10, 4, 3, 9, 15, 6, 12, 1, 11, 8, 2, 14, 7, 5, 13};
 	std::string rev = "2 1 3 5 7 8 6 9 4 11 13 14 12 15 10 ";
@@ -49,6 +52,7 @@ TEST_CASE("TRAVERSAL ORDER REVERSE") {
 	getline(out, result_rev);
 	REQUIRE(rev == result_rev);
 }
+
 TEST_CASE("ADDING NODE") {
     BSTree::Tree tree = { 51, 93, 71, 16, 4, 53, 11, 32};
     tree.insert(80);
