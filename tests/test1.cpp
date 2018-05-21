244a8f796449c8da8e9abbbb41ed78cf0f6df373
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-05-21 22:36:08 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -14,7 +14,7 @@ TEST_CASE("Creating tree2") {
 
 TEST_CASE("TRAVERSAL ORDER DIRECT") {
     	BSTree::Tree tree = { 10, 4, 3, 9, 15, 6, 12, 1, 11, 8, 2, 14, 7, 5, 13};
-    	std::string dir = "10 4 3 1 2 9 6 5 8 7 15 12 11 14 13 ";
+    	std::string dir = "10  4  3  1  2  9  6  5  8  7  15  12  11  14  13  ";
     	std::string str;
     	std::stringstream out(str);
     	tree.print_units(out, BSTree::traversal_order::direct);
@@ -26,7 +26,7 @@ TEST_CASE("TRAVERSAL ORDER DIRECT") {
 
 TEST_CASE("TRAVERSAL ORDER SYMMETRIC") {
 	BSTree::Tree tree = { 10, 4, 3, 9, 15, 6, 12, 1, 11, 8, 2, 14, 7, 5, 13};
-	std::string sym = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ";
+	std::string sym = "1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  ";
 	std::string str;
     	std::stringstream out(str);
 	tree.print_units(out, BSTree::traversal_order::symmetric);
@@ -38,7 +38,7 @@ TEST_CASE("TRAVERSAL ORDER SYMMETRIC") {
 
 TEST_CASE("TRAVERSAL ORDER REVERSE") {
 	BSTree::Tree tree = { 10, 4, 3, 9, 15, 6, 12, 1, 11, 8, 2, 14, 7, 5, 13};
-	std::string rev = "2 1 3 5 7 8 6 9 4 11 13 14 12 15 10 ";
+	std::string rev = "2  1  3  5  7  8  6  9  4  11  13  14  12  15  10  ";
 	std::string str;
     	std::stringstream out(str);
      	tree.print_units(out, BSTree::traversal_order::reverse);
