52da38351f762a15a514dd5af1b9e3558bc71a89
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-05-31 00:41:27 +0300

    Update iterator.cpp

--- examples/iterator.cpp
+++ examples/iterator.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
+#include <string>
 #include "BSTree.hpp"
 using BSTree::Tree;
+
+
 
 int main(int argc, char* argv[])
 {
 	Tree<int> tree{ 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
-	auto it1 = tree.begin();
-	auto it2 = tree.begin();
+	Tree<Student> tree2;
+	Student s1, s2, s3;
+	s1.name = "Sasha";
+	s2.name = "Pasha";
+	s3.name = "Yasha";
+	tree2.insert(s1);
+	tree2.insert(s2);
+	tree2.insert(s3);
 
-	for (; it1 != tree.end(); ++it1) { //первый способ вывода
+
+	auto it1 = tree.begin();
+	auto it2 = tree2.begin();
+
+	for (; it1 != tree.end(); ++it1) {
 		std::cout << *it1 << ' ';
 	}
 	std::cout << std::endl;
-	for (; it2 != tree.end(); ++it2) { //второй способ вывода
-		std::cout << *it2.operator->() << ' ';
+	for (; it2 != tree2.end(); ++it2) {
+		std::cout << it2->name << ' ';
 	}
 	std::cout << std::endl;
-	for (auto& item : tree) {         //третий способ вывода
+	for (auto& item : tree)
 		std::cout << item << " ";
-  }
 	std::cin.get();
 }
